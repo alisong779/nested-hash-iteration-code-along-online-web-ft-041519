b9a41ab3f830e9b58fc670ed3fd20e2bfbbbf0b0
Alison Golden <alisong779@yahoo.com>
2019-04-16 13:52:05 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,28 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
+      knows: nil
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
 
+ contacts.each do |person, data|
+   data.each do |attribute, value|
+   if attribute == :favorite_icecream_flavors
+     value.delete_if do |flavor| flavor == "strawberry"
+  end
+ end
+ end
+ end 
+  contacts
 end
+
